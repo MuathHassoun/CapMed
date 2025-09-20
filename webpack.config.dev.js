d2674a27115d32e51d8790372a5bdff5f55c431c
Muath Hassoun <moath.hassoun@gmail.com>
2025-09-20 22:55:08 +0300

    Edit submit action!

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
+    allowedHosts: "all",
     liveReload: true,
     hot: true,
     open: true,
